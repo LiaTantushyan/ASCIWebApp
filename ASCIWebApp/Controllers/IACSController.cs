565dfe70140dc16b7030ea90a9b7d434461d4f93
lia.2003.tantushyan <lia.2003.tantushyan@gmail.com>
2021-09-23 12:03:08 +0400

    Uploading

--- ASCIWebApp/Controllers/IACSController.cs
+++ ASCIWebApp/Controllers/IACSController.cs
@@ -8,45 +8,62 @@ using System.IO;
 
 namespace ASCIWebApp.Controllers
 {
-	public class IACSController : Controller
-	{
-		private readonly IIACSService _iacsService;
-		public IACSController(IIACSService iacsService)
-		{
-			_iacsService = iacsService;
-		}
+    public class IACSController : Controller
+    {
+        private readonly IIACSService _iacsService;
+        public IACSController(IIACSService iacsService)
+        {
+            _iacsService = iacsService;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> UploadDataFromXml([FromForm] IFormFile file)
-		{
-			if (Path.GetExtension(file.FileName) != ".xml")
-			{
-				ViewData["Message"] = "Please upload Xml,try Again!";
-				return RedirectToAction("Index","IACS");
-			}
+        [HttpPost]
+        public async Task<IActionResult> UploadDataFromXml([FromForm] IFormFile xml, [FromForm] IFormFile xlsx)
+        {
+            if (Path.GetExtension(xml.FileName) != ".xml")
+            {
+                return Json(new
+                {
+                    Message = "Please upload Xml file!",
+                    Succedeed = false
+                });
+            }
+            if (Path.GetExtension(xlsx.FileName) != ".xlsx")
+            {
+                return Json(new
+                {
+                    Message = "Please upload Xlsx file!",
+                    Succedeed = false
+                });
+            }
+            if (xml is null || xml.Length == default)
+            {
+                return Json(new
+                {
+                    Message = "Xml file is null or empty",
+                    Succedeed = false
+                });
+            }
+            if (xlsx is null || xlsx.Length == default)
+            {
+                return Json(new
+                {
+                    Message = "Xlsx file is null or empty",
+                    Succedeed = false
+                });
+            }
+            var data = await _iacsService.GetUsersFromFileAsync(xml);
 
-			else if (file is null || file.Length == default)
-			{
-				return Json(new
-				{
-					Message = "File is null or empty",
-					Succedeed = false
-				});
-			}
-
-			var data = await _iacsService.GetUsersFromFileAsync(file);
-
-			return Json(new
-			{
-				Message = "File was deserialized",
-				Succedeed = true,
-				Value = data
-			});
-		}
-	}
+            return Json(new
+            {
+                Message = "File was deserialized",
+                Succedeed = true,
+                Value = data
+            });
+        }
+    }
 }
